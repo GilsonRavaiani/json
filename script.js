5cdd52d4a16b416745362c12670c790cd8f09ce4
Gilson Ravaiani <gravaiani@yahoo.com.br>
2024-01-13 00:55:43 +0000

    Praticando JSON

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com JSON")
+
+const objetoJS = {
+    nome: 'Gilson Ravaiani',
+    idade: 44,
+    estrangeiro: false
+}
+
+console.log(objetoJS)
+console.log(typeof(objetoJS))
